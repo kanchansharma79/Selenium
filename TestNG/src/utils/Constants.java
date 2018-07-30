65f4c1c489703ae8fdb011248ac6ba864b3825a7
kanchansharma79 <kanchan.sharma@outlook.com>
2018-07-30 00:40:24 -0500

    TestNG Project

--- TestNG/src/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Constants {
-	public static final String TestDataFilePath = "D:\\Workspace\\TestNG\\resources\\";
-	public static final String TestDataFileName= "TestData.xlsx";
-}
